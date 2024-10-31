1857cb010cdcc47688ecc9d104c425144c805384
Developer-Jihad <jihad.developer.bd@gmail.com>
2024-10-31 13:08:50 +0600

    Layout spacing issue solved

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           <DrawerAppBar />
           {children}
           <Footer />
-          <Box sx={{display: { xs: "block", sm: "none" }, height: '50px'}}/>
+          <Box sx={{display: { xs: "block", sm: "none" }, height: '70px'}}/>
           {/* <MobileBottomNav/> */}
         </ThemeToggleProvider>
       </body>
